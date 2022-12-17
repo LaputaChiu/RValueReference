b994ba4abd0a43533135a36be8072bf1af7e238c
GavinChiu <gavinchiu11@gmail.com>
2022-12-17 13:53:34 +0800

    chore: formatting as 4 spaces for tabs

--- rvalue_test.cpp
+++ rvalue_test.cpp
@@ -4,30 +4,30 @@
 
 template <typename T>
 static void CheckType(T&& t) {
-	if constexpr (std::is_rvalue_reference_v<decltype(t)>) {
-		std::cout << "  rvalue reference" << std::endl;
-	}
-	else if constexpr (std::is_lvalue_reference_v<decltype(t)>) {
-		std::cout << "  lvalue reference" << std::endl;
-	}
+    if constexpr (std::is_rvalue_reference_v<decltype(t)>) {
+        std::cout << "  rvalue reference" << std::endl;
+    }
+    else if constexpr (std::is_lvalue_reference_v<decltype(t)>) {
+        std::cout << "  lvalue reference" << std::endl;
+    }
 }
 
 // for only r-value reference
 static void foo(int&& a)
 {
-	std::cout << "      int&&" << std::endl;
+    std::cout << "      int&&" << std::endl;
 }
 
 // for only l-value reference
 static void foo(int& a)
 {
-	std::cout << "      int&" << std::endl;
+    std::cout << "      int&" << std::endl;
 }
 
 // for both l-value and r-value reference
 static void foo(const int& a)
 {
-	std::cout << "      const int&" << std::endl;
+    std::cout << "      const int&" << std::endl;
 }
 
 // for both l-value and r-value reference (extra one copy)
@@ -38,21 +38,21 @@ static void foo(const int& a)
 
 void LambdaTest() {
 
-	auto foo_lambda = [](auto&& arg)
-	{
-		CheckType(std::forward<decltype(arg)>(arg));
+    auto foo_lambda = [](auto&& arg)
+    {
+        CheckType(std::forward<decltype(arg)>(arg));
 
-		foo(arg); // call by lvalue
-		foo(std::forward<decltype(arg)>(arg));  // call by what type it is
-	};
+        foo(arg); // call by lvalue
+        foo(std::forward<decltype(arg)>(arg));  // call by what type it is
+    };
 
-	int x = 1;
+    int x = 1;
 
-	std::cout << "arg type is rvalue" << std::endl;
-	foo_lambda(2);
+    std::cout << "arg type is rvalue" << std::endl;
+    foo_lambda(2);
 
-	std::cout << "arg type is lvalue" << std::endl;
-	foo_lambda(x);
+    std::cout << "arg type is lvalue" << std::endl;
+    foo_lambda(x);
 }
 
 
@@ -60,55 +60,55 @@ void LambdaTest() {
 template <typename T>
 class FooClass {
 private:
-	template <typename KeyType, typename ValueType>
-	void foo_member_internal(KeyType&& key, ValueType&& value) {
-		CheckType(std::forward<KeyType>(key));
-		CheckType(std::forward<ValueType>(value));
-	}
+    template <typename KeyType, typename ValueType>
+    void foo_member_internal(KeyType&& key, ValueType&& value) {
+        CheckType(std::forward<KeyType>(key));
+        CheckType(std::forward<ValueType>(value));
+    }
 
 public:
-	explicit FooClass() = default;
+    explicit FooClass() = default;
 
-	/* key is lvalue const reference (accept lvalue and also rvalue if there's no rvalue reference overloading)
-	 * value is forwarding reference
-	 */
-	template <typename ValueType>
-	void foo_member(const T& key, ValueType&& value) {
-		foo_member_internal(key, value); // call by lvalue
-		foo_member_internal(key, std::forward<ValueType>(value)); // call by what type it is
-	}
+    /* key is lvalue const reference (accept lvalue and also rvalue if there's no rvalue reference overloading)
+     * value is forwarding reference
+     */
+    template <typename ValueType>
+    void foo_member(const T& key, ValueType&& value) {
+        foo_member_internal(key, value); // call by lvalue
+        foo_member_internal(key, std::forward<ValueType>(value)); // call by what type it is
+    }
 
-	/* key type is rvalue reference (accept only rvalue)
-	 * value type is forwarding reference
-	 */
-	template <typename ValueType>
-	void foo_member(T&& key, ValueType&& value) {
-		foo_member_internal(key, value); // call by lvalue
-		foo_member_internal(std::move(key), std::forward<ValueType>(value)); // call by what type it is
-	}
+    /* key type is rvalue reference (accept only rvalue)
+     * value type is forwarding reference
+     */
+    template <typename ValueType>
+    void foo_member(T&& key, ValueType&& value) {
+        foo_member_internal(key, value); // call by lvalue
+        foo_member_internal(std::move(key), std::forward<ValueType>(value)); // call by what type it is
+    }
 
 };
 
 
 int main()
 {
-	int x = 1;
+    int x = 1;
 
-	FooClass<int> fooClass;
+    FooClass<int> fooClass;
 
-	std::cout << "key type is rvalue, value type is rvalue" << std::endl;
-	fooClass.foo_member(2, 2);
+    std::cout << "key type is rvalue, value type is rvalue" << std::endl;
+    fooClass.foo_member(2, 2);
 
-	std::cout << "key type is rvalue, value type is lvalue" << std::endl;
-	fooClass.foo_member(2, x);
+    std::cout << "key type is rvalue, value type is lvalue" << std::endl;
+    fooClass.foo_member(2, x);
 
-	std::cout << "key type is lvalue, value type is rvalue" << std::endl;
-	fooClass.foo_member(x, 2);
+    std::cout << "key type is lvalue, value type is rvalue" << std::endl;
+    fooClass.foo_member(x, 2);
 
-	std::cout << "key type is lvalue, value type is lvalue" << std::endl;
-	fooClass.foo_member(x, x);
+    std::cout << "key type is lvalue, value type is lvalue" << std::endl;
+    fooClass.foo_member(x, x);
 
-	LambdaTest();
+    LambdaTest();
 
-	return 0;
+    return 0;
 }
